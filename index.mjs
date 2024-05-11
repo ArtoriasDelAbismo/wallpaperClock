056892c0462fc775b328cab7f51442e30fcd8c63
Jeronimo <jerojournade1@gmail.com>
2024-05-11 20:26:59 -0300

    initial commit

---
+++ index.mjs
@@ -0,0 +1,24 @@
+import htmlToImage from "node-html-to-image";
+import { readFileSync } from "fs";
+import { setWallpaper } from "wallpaper";
+import cron from "node-cron";
+
+const html = readFileSync("./index.html", (err) => {
+  if (err) {
+    throw err;
+  }
+  return console.log(html);
+});
+
+// cron.schedule("* * * * *", () => {
+  htmlToImage({
+    output: "image.png",
+    html: html.toString()
+  }).then(() => {
+    console.log("Image created");
+    setWallpaper("./image.png").then((err) => {
+        console.log("Wallpaper set succesfully");
+    })
+    })
+  // });
+
